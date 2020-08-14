48c17fad3bf9b6aec44a687450a34965782faee7
atusiniida <atusiniida@gmail.com>
2020-08-14 12:24:21 +0900

    Delete rsync.sh

--- rsync.sh
+++
@@ -1,5 +0,0 @@
-rsync  -av ~/Dropbox/MHBA/R/  sl:MHBA/R
-rsync  -av ~/Dropbox/MHBA/python/  sl:MHBA/python
-rsync  -av ~/Dropbox/MHBA/stan/  sl:MHBA/stan
-rsync  -av ~/Dropbox/MHBA/data/  sl:MHBA/data
-#rsync  -av sl:MHBA/  ~/Dropbox/MHBA
